873a5c23f632b16a455627b3ae21688b9e43fd72
cedrictaro-g <cedric.tarou@gmail.com>
2020-11-22 18:19:13 +0900

    Revert "課題2作成"
    This reverts commit 97db67a6d39c771352e0ff83144b41add1592555.

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.disabled("x-powered-by");
 
-app.use(express.json());
-
 app.use("/public", express.static(__dirname + "/public"));
-
-//ルーティング処理
 app.use("/", require("./routes/index.js"));
-app.use("/test", require("./routes/test.js"));
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,12 @@
 const router = require("express").Router();
 
-let currentUser;
-const changeUser = (body) => {
-  return (currentUser = {
-    name: body.userName,
-    email: body.userEmail,
-    password: body.userPassword,
-  });
-};
-const logoutUser = () => {
-  return (currentUser = {});
-};
-//バリデーション
-const validateRegistData = (body) => {
-  let isValidated = true,
-    errors = {};
-  const regPw = /^([a-zA-Z0-9]{7,})$/;
-  if (!regPw.test(body.userPassword)) {
-    isValidated = false;
-    errors.userPassword = "7文字以上のパスワードを設定してください。";
-  }
-  return isValidated ? undefined : errors;
-};
-
 router.get("/", (req, res) => {
-  res.render("index.ejs", { currentUser });
+    res.render("./index.ejs");
 });
-
 router.get("/login", (req, res) => {
-  res.render("account/login.ejs", { currentUser });
+    res.render("./account/login.ejs");
 });
-
 router.get("/register", (req, res) => {
-  res.render("account/register.ejs", { currentUser });
+    res.render("./account/register.ejs");
 });
-
-// ユーザーを登録する処理
-router.post("/create", (req, res) => {
-  const errors = validateRegistData(req.body);
-  //validationをかける
-  if (errors) {
-    //エラーがあれば移動しない
-    changeUser(req.body);
-    res.render("account/register.ejs", { currentUser, errors });
-    return;
-  } else {
-    //エラーなければ
-    changeUser(req.body);
-    res.redirect("/");
-  }
-});
-
-// logoutの処理
-router.post("/logout", (req, res) => {
-  logoutUser();
-  res.redirect("/");
-});
-
 module.exports = router;
