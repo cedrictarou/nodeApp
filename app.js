9b07fd2bdac0cab0cdf4c80893373ff9d4a3dd76
cedrictaro-g <cedric.tarou@gmail.com>
2020-11-22 18:35:07 +0900

    removed node_modules from git

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.disabled("x-powered-by");
 
+app.use(express.json());
+
 app.use("/public", express.static(__dirname + "/public"));
+
+//ルーティング処理
 app.use("/", require("./routes/index.js"));
 
 app.listen(3000);
