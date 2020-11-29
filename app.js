17d448515b56eee2d75fff791639d0470b8f1269
cedrictaro-g <cedric.tarou@gmail.com>
2020-11-29 10:09:49 +0900

    installed express-validator and fixed other errors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
-app.set("view engine", "ejs");
-app.disabled("x-powered-by");
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.disabled('x-powered-by');
 
 app.use(express.json());
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
 //ルーティング処理
-app.use("/", require("./routes/index.js"));
+app.use('/', require('./routes/index.js'));
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,24 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const userController = require('../controller/userController.js');
+const validatedItems = require('../lib/validation/validate.js');
 
-let currentUser;
-const changeUser = body => {
-  return (currentUser = {
-    name: body.userName,
-    email: body.userEmail,
-    password: body.userPassword,
-  });
-};
-const logoutUser = () => {
-  return (currentUser = {});
-};
-//バリデーション
-const validateRegistData = body => {
-  let isValidated = true,
-    errors = {};
-  const regPw = /^([a-zA-Z0-9]{7,})$/;
-  if (!regPw.test(body.userPassword)) {
-    isValidated = false;
-    errors.userPassword = "7文字以上のパスワードを設定してください。";
-  }
-  if (body.userPassword !== body.userConfirmPassword) {
-    isValidated = false;
-    errors.userPassword = "パスワードが一致しません。";
-  }
-  return isValidated ? undefined : errors;
-};
-
-router.get("/", (req, res) => {
-  res.render("index.ejs", { currentUser });
+router.get('/', (req, res) => {
+  const currentUser = req.query.userName || '';
+  res.render('index.ejs', { currentUser });
 });
 
-router.get("/login", (req, res) => {
-  res.render("account/login.ejs", { currentUser });
+router.get('/login', (req, res) => {
+  res.render('account/login.ejs');
 });
 
-router.get("/register", (req, res) => {
-  res.render("account/register.ejs", { currentUser });
+router.get('/register', (req, res) => {
+  res.render('account/register.ejs');
 });
 
 // ユーザーを登録する処理
-router.post("/create", (req, res) => {
-  const errors = validateRegistData(req.body);
-  const inputData = changeUser(req.body);
-  //validationをかける
-  if (errors) {
-    //エラーがあれば移動しない
-    res.render("account/register.ejs", { inputData, errors });
-    return;
-  } else {
-    //エラーなければ
-    changeUser(req.body);
-    res.redirect("/");
-  }
-});
+router.post('/create', validatedItems, userController.registUser);
 
 // logoutの処理
-router.post("/logout", (req, res) => {
-  logoutUser();
-  res.redirect("/");
-});
+router.post('/logout', userController.logout);
 
 module.exports = router;
